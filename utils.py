fdf891adaa8a2f003498a18c355c0066fc3a1b70
RayVilladiego <silfredomendoza711@gmail.com>
2025-06-23 22:43:22 -0500

    utils.py

--- utils.py
+++ utils.py
@@ -1,9 +1,27 @@
-# utils.py
+from db import obtener_datos
 
-def calcular_estado(total, minimo, maximo):
-    if total < minimo:
+def calcular_estado(producto):
+    df = obtener_datos()
+    fila = df[df['producto'] == producto].iloc[0]
+    total, minimo, maximo = fila['Total'], fila['Min'], fila['Max']
+
+    if total <= minimo:
         return "Bajo"
-    elif total < minimo + 0.2 * (maximo - minimo):
+    elif total <= maximo:
         return "Alerta"
     else:
         return "OK"
+
+def recalcular_estado_global(producto):
+    df = obtener_datos()
+    fila = df[df['producto'] == producto].iloc[0]
+    fisico, minimo, maximo = fila['Fisico'], fila['Min'], fila['Max']
+
+    if fisico <= minimo:
+        estado = "Bajo"
+    elif fisico <= maximo:
+        estado = "Alerta"
+    else:
+        estado = "OK"
+
+    return estado
